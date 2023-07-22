8a83e2c99b212f9da50cc7e8b7e5b8519f43f58a
james collier <astonjames@iahni.com>
2023-07-22 04:52:31 -0600

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]  # Initialize with the first two Fibonacci numbers
+
+    if length < 2:
+        sequence = sequence[:length]  # Adjust the sequence length if it's less than 2
+    else:
+        while len(sequence) < length:  # Generate the Fibonacci sequence up to the desired length
+            next_num = sequence[-1] + sequence[-2]
+            sequence.append(next_num)
+
+    print(sequence)
+
+print_fibonacci(9)
